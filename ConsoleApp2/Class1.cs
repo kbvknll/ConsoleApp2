1d7388aaa75f097c2a5c78f2500499b3e2b59cf7
Екатерина Гордеева <katiyhagordeeva@mail.ru>
2023-10-18 16:49:06 +0300

    221 Гордеева Иванова

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp2
+{
+    public static class ProductFactory
+    {
+        ﻿public static Product Create() 
+        {
+            return new Product();
+        }
+    }
+}
